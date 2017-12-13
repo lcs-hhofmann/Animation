44eac7b8d7889fa38905a127933425aab2517fe7
Hannah Hofmann <hhofmann@lcs.on.ca>
2017-12-13 11:36:32 -0500

    I did the first video with the four circles moving off screen

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    var  y : Int
+    
     // This function runs once
     override init() {
         
@@ -17,17 +19,37 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        y = 250
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        // Clear the background
+        //canvas.fillColor = Color.white
+        //canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
+        
         // Change position
         x += 1
+        y -= 1
         
         // Draw an ellipse in the middle of the canvas
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        
+        canvas.fillColor = Color.init(hue: 32, saturation: 65, brightness: 89, alpha: 100)
+        canvas.drawEllipse(centreX: y, centreY: 350, width: 50, height: 50)
+        
+        canvas.fillColor = Color.green
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: y, centreY: 150, width: 50, height: 50)
+        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
         
     }
     
